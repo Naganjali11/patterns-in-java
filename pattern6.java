0889bf73d93e0f7ab887f6c88605170deaf48c0a
Naganjali11 <naganjali425@gmail.com>
2025-09-06 14:06:42 +0530

    javafullstackprograms

---
+++ pattern6.java
@@ -0,0 +1,83 @@
+package com.codegnan.patternexamples;
+
+public class pattern6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(j+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((6-j)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((6-i)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(96+j)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(96+i)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(102-j)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+		for(int i=1;i<=5;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print((char)(102-i)+" ");
+			}
+			System.out.println();
+			
+		}
+		System.out.println();
+
+	}
+
+}
